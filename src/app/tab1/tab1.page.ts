425088c06f148216dcf3844a447ea1f735bfebaa
victorb-s <114593367+victorb-s@users.noreply.github.com>
2023-06-10 19:55:27 -0300

    Initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page {
+  constructor() {}
+
+  option = {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 10,
+  };
+}
